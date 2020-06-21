fbbd016de005fb277e07edf2a4470dd61d89b24a
andvis <45904930+avistavkin@users.noreply.github.com>
2020-06-21 16:55:15 +0300

    Initial Commit

---
+++ vNextChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vNextChat
+//
+//  Created by AndreyV on 21.06.2020.
+//  Copyright © 2020 AndreyV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
